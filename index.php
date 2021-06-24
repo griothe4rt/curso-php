c6ebc8dff107dc6b10d6927e670d476cfdcb0238
Lucas Bomfim <lucascostabomfim5@gmail.com>
2021-06-24 16:55:02 -0300

    alterando nome da pasta e arquivo css

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <link href="https://fonts.googleapis.com/css?family=Oswald:200,300,400,500,600,700" rel="stylesheet">
-    <link rel="stylesheet" href="recursos/css/estilo.css">
+    <link rel="stylesheet" href="assets/css/style.css">
     <title>Curso PHP</title>
 </head>
 <body>
